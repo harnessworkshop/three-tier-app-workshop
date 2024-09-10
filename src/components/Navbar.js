d8a0d616ba9e19fc2edd7d3bd93928cbb77db811
hassan <hassan.saab@harness.io>
2024-09-10 10:10:31 -0400

    Clean up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,6 @@ import harnessLogo from '../harness-logo-white.png';
 import '../App.css'
 
 const Navbar = ({ }) => {
-  // Determines if the "menu icon" was clicked or not. Note that this icon is only visible when the window width is small.
-  const [menuClicked, setMenuClicked] = useState(false);
-
-  const toggleMenuClick = () => {
-    setMenuClicked(!menuClicked);
-  };
-  const navbarLinks = ["one", "two"];
-  const track = useTrack();
   return (
     <nav className="navbar">
       <img src={harnessLogo} className="navbar__logo" />
