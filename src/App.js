24af2e3336de08c81d8f231b93feb16d93c703e8
hassan <hassan.saab@harness.io>
2024-11-14 14:41:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
-import { useEffect } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
-import { useSplitTreatments } from '@splitsoftware/splitio-react';
 import travel_01 from "./travel-01.jpeg";
 import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
-  const { treatments: { background_color } } = useSplitTreatments({ names: ['background_color'], updateOnSdkUpdate: true });
-
   let divStyle = {
-    backgroundColor: background_color.treatment || "gray"
+    // set background color to transparent
+    backgroundColor: "transparent"
   }
-  let imageSrc = background_color.treatment === "transparent" ? true : false;
-
-  useEffect(() => {
-    console.log("update");
-    console.log(imageSrc);
-  }, [background_color.treatment])
-
-  if(imageSrc) { 
-    return (
-      <div style={divStyle}>
-        <Navbar/>
-        <div className="hero">
-          <img src={travel_01} alt="Travel" className="hero__image" />
-          <div className="hero__qr">
-            <QRCodeSVG value={window.location.href} />
-          </div>
-          <h1 className="hero__title">Devops made simple.</h1>
-        </div>
-      </div>
-    )
-  } 
 
   return (
     <div style={divStyle}>
       <Navbar/>
       <div className="hero">
+        <img src={travel_01} alt="Travel" className="hero__image" />
         <div className="hero__qr">
           <QRCodeSVG value={window.location.href} />
         </div>
         <h1 className="hero__title">Devops made simple.</h1>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
