a096be68f641e8e90482990d5e671314d341e37e
hsaab <hassan.saab@harness.io>
2025-02-27 15:55:32 -0500

    update rds output

--- {{cookiecutter.app_slug}}/infrastructure/modules/rds/outputs.tf
+++ {{cookiecutter.app_slug}}/infrastructure/modules/rds/outputs.tf
@@ -1,6 +1,6 @@
 output "db_instance_endpoint" {
   description = "The database instance endpoint without port"
-  value       = aws_db_instance.database.address
+  value       = aws_db_instance.main.address
 }
 
 output "db_instance_id" {
